6b252ddbedf970608a1d9f758246e5bb692efd47
YiuChoi <whucaiyao@gmail.com>
2016-08-24 07:51:43 +0800

    perfect

--- app/src/main/java/name/caiyao/wechatshake/MainHook.java
+++ app/src/main/java/name/caiyao/wechatshake/MainHook.java
@@ -1,15 +1,20 @@
 package name.caiyao.wechatshake;
 
-import android.os.Build;
-import android.view.KeyEvent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 
 import java.util.Random;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedBridge.hookAllMethods;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
 
 /**
  * Created by 蔡小木 on 2016/8/23 0023.
@@ -33,39 +38,6 @@ public class MainHook implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        if (loadPackageParam.packageName.equals("android")) {
-            final Class<?> phoneWindowManager;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                phoneWindowManager = XposedHelpers.findClass("com.android.server.policy.PhoneWindowManager",
-                        loadPackageParam.classLoader);
-                XposedBridge.hookAllMethods(phoneWindowManager, "interceptKeyBeforeQueueing", new XC_MethodHook() {
-
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        int v1 = ((KeyEvent) param.args[0]).getKeyCode();
-                        if (v1 == KeyEvent.KEYCODE_VOLUME_DOWN) {
-                            isShake = true;
-                            count = 1;
-                        }
-                    }
-                });
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                phoneWindowManager = XposedHelpers.findClass("com.android.internal.policy.impl.PhoneWindowManager", loadPackageParam.classLoader);
-                XposedBridge.hookAllMethods(phoneWindowManager, "interceptKeyBeforeQueueing", new XC_MethodHook() {
-
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        int v1 = (int) param.args[3];
-                        XposedBridge.log("KeyCode:" + v1);
-                        if (v1 == KeyEvent.KEYCODE_VOLUME_DOWN) {
-                            isShake = true;
-                            count = 1;
-                        }
-                    }
-                });
-            }
-        }
-
         if (loadPackageParam.packageName.equals(packages[0]) ||
                 loadPackageParam.packageName.equals(packages[1]) ||
                 loadPackageParam.packageName.equals(packages[2]) ||
@@ -76,8 +48,17 @@ public class MainHook implements IXposedHookLoadPackage {
                 loadPackageParam.packageName.equals(packages[7]) ||
                 loadPackageParam.packageName.equals(packages[8]) ||
                 loadPackageParam.packageName.equals(packages[9]) || loadPackageParam.packageName.equals("com.tencent.mm")) {
-            final Class<?> sensorEL = XposedHelpers.findClass("android.hardware.SystemSensorManager$SensorEventQueue", loadPackageParam.classLoader);
-            XposedBridge.hookAllMethods(sensorEL, "dispatchSensorEvent", new XC_MethodHook() {
+            final Object activityThread = XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.app.ActivityThread", null), "currentActivityThread");
+            final Context systemContext = (Context) XposedHelpers.callMethod(activityThread, "getSystemContext");
+            systemContext.registerReceiver(new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    isShake = true;
+                    count = 1;
+                }
+            }, new IntentFilter("name.caiyao.START"));
+            final Class<?> sensorEL = findClass("android.hardware.SystemSensorManager$SensorEventQueue", loadPackageParam.classLoader);
+            hookAllMethods(sensorEL, "dispatchSensorEvent", new XC_MethodHook() {
 
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
@@ -88,6 +69,12 @@ public class MainHook implements IXposedHookLoadPackage {
                             isShake = false;
                         }
                     }
+                }
+            });
+            findAndHookMethod(loadPackageParam.packageName + ".plugin.shake.ui.ShakeReportUI", loadPackageParam.classLoader, "onResume", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    systemContext.sendBroadcast(new Intent("name.caiyao.START"));
                 }
             });
         }
