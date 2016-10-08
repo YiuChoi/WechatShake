add79678a907eae6d1e48c0acfe4d9eccd07c819
YiuChoi <whucaiyao@gmail.com>
2016-10-08 17:27:03 +0800

    perfect

---
+++ app/src/main/java/name/caiyao/wechatshake/MainActivity.java
@@ -0,0 +1,90 @@
+package name.caiyao.wechatshake;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Button btn_floatView = new Button(getApplicationContext());
+                btn_floatView.setText("摇一摇");
+
+                final WindowManager wm = (WindowManager) getApplicationContext().getSystemService(
+                        Context.WINDOW_SERVICE);
+                final WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+
+                // 设置window type
+                params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        /*
+         * 如果设置为params.type = WindowManager.LayoutParams.TYPE_PHONE; 那么优先级会降低一些,
+         * 即拉下通知栏不可见
+         */
+
+                params.format = PixelFormat.RGBA_8888; // 设置图片格式，效果为背景透明
+
+                // 设置Window flag
+                params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        /*
+         * 下面的flags属性的效果形同“锁定”。 悬浮窗不可触摸，不接受任何事件,同时不影响后面的事件响应。
+         * wmParams.flags=LayoutParams.FLAG_NOT_TOUCH_MODAL |
+         * LayoutParams.FLAG_NOT_FOCUSABLE | LayoutParams.FLAG_NOT_TOUCHABLE;
+         */
+
+                // 设置悬浮窗的长得宽
+                params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+                params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+                // 设置悬浮窗的Touch监听
+                btn_floatView.setOnTouchListener(new View.OnTouchListener() {
+                    int lastX, lastY;
+                    int paramX, paramY;
+
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                lastX = (int) event.getRawX();
+                                lastY = (int) event.getRawY();
+                                paramX = params.x;
+                                paramY = params.y;
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+                                int dx = (int) event.getRawX() - lastX;
+                                int dy = (int) event.getRawY() - lastY;
+                                params.x = paramX + dx;
+                                params.y = paramY + dy;
+                                // 更新悬浮窗位置
+                                wm.updateViewLayout(btn_floatView, params);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+
+                btn_floatView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        v.getContext().sendBroadcast(new Intent().setAction("name.caiyao.START"));
+                    }
+                });
+                wm.addView(btn_floatView, params);
+            }
+        });
+    }
+}
--- app/src/main/java/name/caiyao/wechatshake/MainHook.java
+++ app/src/main/java/name/caiyao/wechatshake/MainHook.java
@@ -4,10 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Build;
-import android.view.KeyEvent;
 
-import java.lang.reflect.Field;
 import java.util.Random;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -23,53 +20,11 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class MainHook implements IXposedHookLoadPackage {
     private int count = 1;
     private static boolean isShake = false;
-    private String[] packages = {
-            "com.tencent.vt11",
-            "com.tencent.vt12",
-            "com.tencent.vt13",
-            "com.tencent.vt14",
-            "com.tencent.vt15",
-            "com.tencent.vt16",
-            "com.tencent.vt17",
-            "com.tencent.vt18",
-            "com.tencent.vt19",
-            "com.tencent.vt20"
-    };
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        if (loadPackageParam.packageName.equals("android")) {
-            final Class<?> phoneWindowManager;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                phoneWindowManager = XposedHelpers.findClass("com.android.server.policy.PhoneWindowManager", loadPackageParam.classLoader);
-            } else {
-                phoneWindowManager = XposedHelpers.findClass("com.android.internal.policy.impl.PhoneWindowManager", loadPackageParam.classLoader);
-            }
-            XposedBridge.hookAllMethods(phoneWindowManager, "interceptKeyBeforeQueueing", new XC_MethodHook() {
 
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    int v1 = ((KeyEvent) param.args[0]).getKeyCode();
-                    Field contextField = XposedHelpers.findField(phoneWindowManager, "mContext");
-                    contextField.setAccessible(true);
-                    final Context context = (Context) contextField.get(param.thisObject);
-                    if (v1 == KeyEvent.KEYCODE_K) {
-                        context.sendBroadcast(new Intent("name.caiyao.START"));
-                    }
-                }
-            });
-        }
-
-        if (loadPackageParam.packageName.equals(packages[0]) ||
-                loadPackageParam.packageName.equals(packages[1]) ||
-                loadPackageParam.packageName.equals(packages[2]) ||
-                loadPackageParam.packageName.equals(packages[3]) ||
-                loadPackageParam.packageName.equals(packages[4]) ||
-                loadPackageParam.packageName.equals(packages[5]) ||
-                loadPackageParam.packageName.equals(packages[6]) ||
-                loadPackageParam.packageName.equals(packages[7]) ||
-                loadPackageParam.packageName.equals(packages[8]) ||
-                loadPackageParam.packageName.equals(packages[9]) || loadPackageParam.packageName.equals("com.tencent.mm")) {
+        if (loadPackageParam.packageName.equals("com.tencent.mm")) {
             final Object activityThread = XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.app.ActivityThread", null), "currentActivityThread");
             Context systemContext = (Context) XposedHelpers.callMethod(activityThread, "getSystemContext");
             systemContext.registerReceiver(new BroadcastReceiver() {
